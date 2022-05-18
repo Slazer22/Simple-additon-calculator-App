59736e4bd27c54b279b0f046593c82ca2f498e64
Slazer22 <48864818+Slazer22@users.noreply.github.com>
2022-05-17 22:58:16 -0500

    Add project files.

---
+++ Excerise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+/* This program will Display the calculation from two values. Two inputs will be prompted*/
+
+
+namespace Excercise;
+
+
+class Calculator
+{
+
+    public static void Main(String[] args)
+    {
+
+        //Declaring variables
+        int num1, num2;
+        int totalsum;
+
+        Console.WriteLine("Please enter the first number"); //prompts user to enter the first number.
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please enter the second number"); //prompts user to enter the second number.
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        totalsum = num1 + num2;
+
+        Console.WriteLine("The sum between the two numbers " + totalsum);
+        Console.ReadKey();
+
+
+    }
+
+
+
+}
